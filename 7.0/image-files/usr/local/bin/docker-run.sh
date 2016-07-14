4c84af16ab2274b432f6e58bb6078823b78742a8
Tobias Munk <schmunk@usrbin.de>
2016-07-14 03:13:23 +0200

    added PHP_ENABLE_XDEBUG variable

--- 7.0/image-files/usr/local/bin/docker-run.sh
+++ 7.0/image-files/usr/local/bin/docker-run.sh
@@ -11,5 +11,10 @@ sed -i "s|\${DISPLAY_PHP_ERRORS}|${DISPLAY_PHP_ERRORS}|" /usr/local/etc/php/conf
 # Work around permission errors locally TODO only if local
 usermod -u ${PHP_USER_ID} www-data
 
+if [ -n "$PHP_ENABLE_XDEBUG" ] ; then
+    docker-php-ext-enable xdebug
+fi
+sed -i "s|\${FASTCGI_PASS_HOST}|${FASTCGI_PASS_HOST}|" /etc/nginx/nginx.conf
+
 # Run the command sent as command line arguments
 php-fpm
