488aa76141235e4d2964a0d2d475b48d21bb97bb
Tobias Munk <schmunk@usrbin.de>
2016-10-05 21:38:20 +0200

    added entrypoint script

---
+++ 7.0/image-files/usr/local/bin/docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script is run within the php containers on start
+
+# Fail on any error
+set -o errexit
+
+# Setup config variables only available at runtime
+sed -i "s|\${DISPLAY_PHP_ERRORS}|${DISPLAY_PHP_ERRORS}|" /usr/local/etc/php/conf.d/app.ini
+
+# Set permissions based on ENV variable
+usermod -u ${PHP_USER_ID} www-data
+
+# Enable xdebug by ENV variable
+if [ -n "${PHP_ENABLE_XDEBUG}" ] ; then
+    docker-php-ext-enable xdebug
+    echo "Enabled xdebug"
+fi
+
+# Execute CMD
+exec "$@"
--- 7.0/image-files/usr/local/bin/docker-run.sh
+++ 7.0/image-files/usr/local/bin/docker-run.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
 # This script is run within the php containers on start
+# docker-run.sh is available for legacy setups
 
 # Fail on any error
 set -o errexit
-
-# Setup config variables only available at runtime
-sed -i "s|\${DISPLAY_PHP_ERRORS}|${DISPLAY_PHP_ERRORS}|" /usr/local/etc/php/conf.d/app.ini
-
-# Work around permission errors locally TODO only if local
-usermod -u ${PHP_USER_ID} www-data
-
-# Enable xdebug by ENV variable
-if [ -n "${PHP_ENABLE_XDEBUG}" ] ; then
-    docker-php-ext-enable xdebug
-    echo "Enabled xdebug"
-fi
 
 # Run the command sent as command line arguments
 php-fpm
