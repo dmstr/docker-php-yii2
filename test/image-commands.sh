31c85e09ffa00554d526d9cc3ccf78a41f28399a
Tobias Munk <schmunk@usrbin.de>
2016-12-08 22:45:40 +0100

    added git test

--- test/image-commands.sh
+++ test/image-commands.sh
@@ -7,3 +7,4 @@ docker-compose run --rm ${PHP_SERVICE} composer --version
 docker-compose run --rm ${PHP_SERVICE} codecept --version
 docker-compose run --rm ${PHP_SERVICE} phpunit --version
 docker-compose run --rm ${PHP_SERVICE} mysql --version
+docker-compose run --rm ${PHP_SERVICE} git --version
