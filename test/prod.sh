8d6cde91bdc0b9e3eb29ea5f967b01ef07f96577
Jens Giessmann <j.giessmann@herzogkommunikation.de>
2017-02-02 17:06:38 +0100

    changed 'is opcache loaded test'

--- test/prod.sh
+++ test/prod.sh
@@ -3,4 +3,8 @@
 PHP_SERVICE=${PHP_SERVICE-php}
 
 docker-compose run --rm ${PHP_SERVICE} php --version
-docker-compose run --rm ${PHP_SERVICE} php -i | grep opcache
+# this would match even if module is not loaded, eg. when commented out in opcache.ini
+# docker-compose run --rm ${PHP_SERVICE} php -i | grep opcache
+
+# better check loaded module list
+docker-compose run --rm ${PHP_SERVICE} php -m | grep -i opcache
