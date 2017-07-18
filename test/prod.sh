59262ceea6b03800f1bf21fed1b2d8c344fd3f96
Tobias Munk <schmunk@usrbin.de>
2017-07-18 11:47:57 +0200

    updated alpine packages, refactored build

--- test/prod.sh
+++ test/prod.sh
@@ -7,7 +7,9 @@ docker-compose run --rm ${PHP_SERVICE} php --version
 # docker-compose run --rm ${PHP_SERVICE} php -i | grep opcache
 
 # better check loaded module list
-docker-compose run --rm ${PHP_SERVICE} php -m | grep -i opcache
+docker-compose run --rm ${PHP_SERVICE} php -m | grep -i gd 1>/dev/null || exit 1
+docker-compose run --rm ${PHP_SERVICE} php -m | grep -i mcrypt 1>/dev/null || exit 1
+docker-compose run --rm ${PHP_SERVICE} php -m | grep -i opcache 1>/dev/null || exit 1
 
 # check composer auth
 docker-compose run -e GITHUB_API_TOKEN=abcd12345 --rm ${PHP_SERVICE} sh -c '\
