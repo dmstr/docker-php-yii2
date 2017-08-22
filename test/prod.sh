30e5507c19ca29c4364f59fbf184b9b872a0396b
Tobias Munk <schmunk@usrbin.de>
2017-08-22 16:21:21 +0200

    enabled mcrypt; updated check messages

--- test/prod.sh
+++ test/prod.sh
@@ -6,13 +6,19 @@ docker-compose run --rm ${PHP_SERVICE} php --version
 # this would match even if module is not loaded, eg. when commented out in opcache.ini
 # docker-compose run --rm ${PHP_SERVICE} php -i | grep opcache
 
-# better check loaded module list
+echo "Checking GDlib..."
 docker-compose run --rm ${PHP_SERVICE} php -m | grep -i gd 1>/dev/null || exit 1
+
+echo "Checking mcrypt..."
 docker-compose run --rm ${PHP_SERVICE} php -m | grep -i mcrypt 1>/dev/null || exit 1
+
+echo "Checking opcache..."
 docker-compose run --rm ${PHP_SERVICE} php -m | grep -i opcache 1>/dev/null || exit 1
 
-# check composer auth
+echo "Checking composer auth..."
 docker-compose run -e GITHUB_API_TOKEN=abcd12345 --rm ${PHP_SERVICE} sh -c '\
     composer && \
     [ -f ~/.composer/auth.json ] || exit 1 \
     '
+
+echo "Done."
