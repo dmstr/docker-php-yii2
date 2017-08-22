0e5d8bd56372dd78f70ca9e9198bc83768b9295f
Tobias Munk <schmunk@usrbin.de>
2017-08-22 16:35:08 +0200

    updated nginx tests

--- test/debug.sh
+++ test/debug.sh
@@ -3,15 +3,16 @@
 PHP_SERVICE=${PHP_SERVICE-php}
 
 echo "Running one-off command with xdebug"
-docker-compose run -e PHP_ENABLE_XDEBUG=1 ${PHP_SERVICE} php -i | grep xdebug
+PHP_ENABLE_XDEBUG=1 docker-compose run ${PHP_SERVICE} php -i | grep xdebug
 
+echo ""
 echo "Starting stack with xdebug"
 PHP_ENABLE_XDEBUG=1 docker-compose up -d ${PHP_SERVICE}
 sleep 5
 docker-compose exec ${PHP_SERVICE} php -i -- | grep xdebug
 docker-compose down -v
 
-
+echo ""
 echo "Starting stack without xdebug"
 PHP_ENABLE_XDEBUG=0 docker-compose up -d ${PHP_SERVICE}
 sleep 5
